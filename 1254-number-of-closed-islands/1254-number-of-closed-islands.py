97a9f8d3f2c617889e1212e1db2b2d20ea413b9d
MeenakshiKathiresan <meenakshikathiresan3@gmail.com>
2023-03-09 11:32:18 -0500

    Time: 139 ms (13.85%), Space: 14.7 MB (8.46%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def closedIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+        visited = set()
+        
+        def dfs(grid, i, j):
+            
+            
+            directions = [[0,1],[0,-1],[1,0],[-1,0]]
+            
+            if 0 <= i < len(grid) and 0<= j < len(grid[0]):
+        
+                if (i,j) in visited or grid[i][j] == 1: return True
+                else:
+                        visited.add((i,j))
+                        isClosed = True
+                        # if i == 0 or i == len(grid)-1 or j == 0 or j == len(grid[0])-1:
+                        #     isEdge =  True
+                            
+                        for direction in directions:
+                            currI = i + direction[0]
+                            currJ = j + direction[1]
+                            
+                            
+                            if  not dfs(grid, currI, currJ):
+                                isClosed = False
+                        return isClosed
+            else:
+                return False
+        
+        count = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0 and (i,j) not in visited and dfs(grid, i, j):
+                    count+=1
+                    
+        return count
+                            
+                            
+                            
+                            
+                            
+                            
+                    
