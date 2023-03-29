ff7aa904669e07dc8527df48ddade7c0435809fd
MeenakshiKathiresan <meenakshikathiresan3@gmail.com>
2023-03-29 11:38:11 -0400

    Time: 46 ms (58.23%), Space: 14 MB (25.00%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,17 +1,8 @@
 class Solution:
-    def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        satisfaction.sort()
-        
-        def dp(index, time):
-            if index > len(satisfaction) -1: 
-                return 0 
-            
-            if (index, time) in memo: 
-                return memo[(index, time)]
-            
-            memo[(index, time)] = max(satisfaction[index]*time + dp(index +1, time +1), dp(index + 1, time))
-            
-            return memo[(index, time)]
-        
-        memo = {} 
-        return dp(0,1)
+    def maxSatisfaction(self, A: List[int]) -> int:
+        res = total = 0
+        A.sort()
+        while A and A[-1] + total > 0:
+            total += A.pop()
+            res += total
+        return res
