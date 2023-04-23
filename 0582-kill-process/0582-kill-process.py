25349edb6a90bf5683e503620e1f48156a65b068
MeenakshiKathiresan <meenakshikathiresan3@gmail.com>
2023-04-23 11:17:07 -0400

    Time: 358 ms (93.55%), Space: 23.2 MB (35.48%) - LeetHub

---
+++ 0582-kill-process/0582-kill-process.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def killProcess(self, pid, ppid, kill):
+        """
+        :type pid: List[int]
+        :type ppid: List[int]
+        :type kill: int
+        :rtype: List[int]
+        """
+        
+        # graph:
+        #     1 : 
+        #     3 : [1, 5]
+        #     10 : 
+        #     5 : [10]
+                
+        graph = {}
+        graph[0] =[]
+        
+        for p in pid:
+            graph[p] = []
+        
+        for i, p in enumerate(ppid):
+            graph[p].append(pid[i])
+            
+        res = [kill]
+        queue = [kill]
+        
+        while queue:
+            curr = queue.pop()
+            
+            for child in graph[curr]:
+                res.append(child)
+                queue.append(child)
+                
+        return res
