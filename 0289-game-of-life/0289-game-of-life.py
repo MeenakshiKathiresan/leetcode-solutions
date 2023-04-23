0870b346ae62954198427f0cd5a39c74c47106ba
MeenakshiKathiresan <meenakshikathiresan3@gmail.com>
2023-04-23 11:04:44 -0400

    Time: 23 ms (40.00%), Space: 13.4 MB (73.71%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def gameOfLife(self, new_board):
+        """
+        :type board: List[List[int]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        
+        
+        board = copy.deepcopy(new_board)
+        
+        def getNeighborCount(i,j):
+            count = 0
+            directions = [[0,1],[0,-1],[1,0],[-1,0],[-1,1],[1,1],[1,-1],[-1,-1]]
+            
+            for direction in directions:
+                curr_i = i + direction[0]
+                curr_j = j + direction[1]
+                if 0 <= curr_i < len(board) and 0 <= curr_j < len(board[0]):
+                    count += board[curr_i][curr_j]
+            
+            return count
+        
+        for i, row in enumerate(board):
+            for j, cell in enumerate(row):
+                neigh = getNeighborCount(i,j)
+                curr = board[i][j]
+                if board[i][j] == 1:
+                    if neigh < 2 or neigh > 3:
+                        curr = 0
+                else:
+                    if neigh == 3:
+                        curr = 1
+                new_board[i][j] = curr
+               
+       
+        print(board)
+        return new_board
