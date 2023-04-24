5a2cfe06232a6846bc6c53ab323edad0e612defd
MeenakshiKathiresan <meenakshikathiresan3@gmail.com>
2023-04-24 05:34:25 -0400

    Time: 15 ms (87.43%), Space: 13.6 MB (12.43%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        
+        stones.sort()
+        
+        
+        while len(stones) > 1:
+            stone_1 = stones.pop()
+            stone_2 = stones.pop()
+            if stone_1 != stone_2:
+                bisect.insort(stones, stone_1 - stone_2)
+        return stones[0] if stones else 0
