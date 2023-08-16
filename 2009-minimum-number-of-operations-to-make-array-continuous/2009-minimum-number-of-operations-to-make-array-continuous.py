7212fa6f0b622ac1c23618802059a71740b5524f
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-08-16 17:59:33 -0400

    Time: 686 ms (84.15%), Space: 35.3 MB (75.61%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
@@ -1,9 +1,7 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         total_len = len(nums) 
-        print(total_len)
         nums = sorted(set(nums))  
-        print(nums)
         n = len(nums)
         l, r = 0, 0
         max_window = 1
@@ -14,5 +12,4 @@ class Solution:
             else:
                 l += 1
             max_window = max(max_window, r - l)
-        print(max_window)
         return total_len - max_window
