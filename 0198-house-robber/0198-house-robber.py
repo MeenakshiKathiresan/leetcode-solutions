f693a6e08f8c2ec4a291b299119a3ee45e2ef652
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-08-31 11:19:14 -0400

    Time: 35 ms (87.80%), Space: 16.2 MB (93.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,20 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
         dp = [-1] * len(nums)
-        def traverse(i):
+        
+        dp[0] = nums[0]
+        
+        
+        
+        for i in range(1, len(nums)):
             
-            if i < 0:
-                return 0
-            if i == 0:
-                return nums[i]
-            if dp[i] != -1:
-                return dp[i]
-            
-            take = nums[i] + traverse(i-2)
-            not_take = traverse(i-1)
+            take = nums[i]
+            if i > 1:
+                take = nums[i] + dp[i-2]
+                
+            not_take = dp[i-1]
             
             dp[i] = max(take, not_take)
-            return dp[i]
-        
-        return traverse(len(nums)-1)
+            
+        return dp[len(nums)-1]
