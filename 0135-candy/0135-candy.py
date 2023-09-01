5a7112e5420d3087a5a2ff7bbd011d75f3df3619
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-09-01 15:06:48 -0400

    Time: 145 ms (69.71%), Space: 19.1 MB (99.33%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,17 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        n = len(ratings)
+        res = [1] * n
+        
+        # left to right
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                res[i] = res[i-1] + 1
+        
+        # right to left
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                res[i] = max(res[i], res[i+1] + 1)
+                
+        return sum(res)
