b4370ef8be5ab5fdb6547be34efbb67bc77c7ee1
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-09-08 07:57:58 -0400

    Time: 74 ms (76.10%), Space: 17 MB (29.72%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,16 +1,18 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        timestamp = []
-        for trip in trips:
-            timestamp.append([trip[1], trip[0]])
-            timestamp.append([trip[2], -trip[0]])
-
-        timestamp.sort()
-
-        used_capacity = 0
-        for time, passenger_change in timestamp:
-            used_capacity += passenger_change
-            if used_capacity > capacity:
+        
+        trips.sort(key = lambda x:x[1])
+        curr_car = 0
+        heap = []
+        
+        for i, trip in enumerate(trips):
+            while len(heap) > 0 and heap[0][0] <= trip[1]:
+                    curr_car -= heap[0][1]
+                    heapq.heappop(heap)
+            if (curr_car + trip[0] <= capacity):
+                curr_car += trip[0]
+                heapq.heappush(heap, [trip[2],trip[0]])
+            else:
                 return False
-
         return True
+            
