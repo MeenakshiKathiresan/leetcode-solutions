25daacc94f6f2fff1c5af062eed818f3fbef896a
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-09-23 22:11:34 -0400

    Time: 82 ms (78.13%), Space: 16.4 MB (48.67%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,9 +1,5 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        # numerator - find the pascal value of the element in the triangle
-        # denominator - find denominator which is 2 pow row
-        # poured - 2 pow row -1 => remaining
-        # return => remaining * num/den 
         
         rows = [[0] * (i+1) for i in range(101)]
         rows[0][0] = poured
@@ -19,20 +15,3 @@ class Solution:
         return min(1, rows[query_row][query_glass])
                     
         
-#         denom = pow(2, query_row)
-        
-#         rows = []
-#         for index in range(query_row):
-#             rows.append([])
-#             curr_sum = 0
-#             for i in range(index+1):
-#                 if i == 0 or i == index:
-#                     rows[index].append(1)
-#                 else:
-#                     no = rows[index-1][i-1] + rows[index-1][i]
-#                     rows[index].append(no)
-#         print(rows)
-#         numerator = rows
-        
-#         return 0
- 
