80c3793d049063eac592f76fb23f84eea4a4d447
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-09-26 18:03:46 -0400

    Time: 130 ms (46.27%), Space: 42.8 MB (74.81%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int pointer = 0;
+        for(int i =0; i<nums.Length; i++){
+            if (nums[i] != val){
+                nums[pointer] = nums[i];
+                pointer++;
+            }
+        }
+    
+        return pointer;
+    }
+}
