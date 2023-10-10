c04ffbffe141d39246280da759a341621b07efac
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-10-10 10:59:10 -0400

    Time: 49 ms (47.77%), Space: 16.5 MB (24.40%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,23 +4,46 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        n = len(nums)
-        if n <= 1:
+        # => traverse reverse
+        # => if in ascending, simple, just switch the first pair of ascending
+        # => if descending, get the whole descending section, get the next biggest compared to left of the descending section. Next biggest is kept in place of left, left + all elements of descending are sorted 
+        # => if fully in reverse sorted, return reverse
+        
+        nd = len(nums) - 1 
+        cmp_nd = nd - 1 
+        if nums[nd] > nums[cmp_nd]:
+            temp = nums[nd]
+            nums[nd] = nums[cmp_nd]
+            nums[cmp_nd] = temp
             return
-        
-        i = n - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
-            i -= 1
-        
-        if i >= 0:
-            j = n - 1
-            while nums[j] <= nums[i]:
-                j -= 1
-            
-            nums[i], nums[j] = nums[j], nums[i]
-        
-        left, right = i + 1, n - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        else:
+            # get the descending section
+            start = nd
+            while cmp_nd >= 0 and nums[nd] <= nums[cmp_nd]:
+                cmp_nd -= 1
+                nd -= 1
+
+            if cmp_nd == -1:
+                print("rev")
+                nums[:] = nums[::-1]
+            else:
+                target = nums[cmp_nd]
+                print(cmp_nd)
+                next_element = 101
+                next_element_nd = -1
+                for i in range(cmp_nd+1, start+1):
+                    if nums[i] > target:
+                        if nums[i] < next_element:
+                            next_element = nums[i]
+                            next_element_nd = i
+                
+                # switch next element and cmp_nd
+                temp = nums[cmp_nd]
+                nums[cmp_nd] = nums[next_element_nd]
+                nums[next_element_nd] = temp
+                
+                # sort cmp_nd + 1 to end
+                #nums = nums[:cmp_nd+1] + sorted(nums[cmp_nd+1:])
+                nums[cmp_nd+1:] = sorted(nums[cmp_nd+1:])
+                    
+     
