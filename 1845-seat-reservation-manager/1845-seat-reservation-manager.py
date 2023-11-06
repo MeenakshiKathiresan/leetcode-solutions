b2c8498d3cba488246fa52cccb6313ab27ecf95f
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2023-11-06 08:41:08 -0500

    Time: 369 ms (98.12%), Space: 42.5 MB (92.09%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.py
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.py
@@ -3,12 +3,14 @@ class SeatManager:
 
     def __init__(self, n: int):
         self.heap = []
-        for i in range(1, n+1):
-            heappush(self.heap, i)
+        self.pointer = 1
         
 
     def reserve(self) -> int:
-        return heappop(self.heap)
+        if self.heap:
+            return heappop(self.heap)
+        self.pointer += 1
+        return self.pointer - 1
         
 
     def unreserve(self, seatNumber: int) -> None:
