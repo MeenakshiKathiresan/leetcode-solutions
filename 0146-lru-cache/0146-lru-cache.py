a2e1fc3de49d6c851afbfe44d194cfe18d859a02
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-02-15 12:51:49 -0500

    Time: 260 ms (7.6%), Space: 78.5 MB (46.05%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,27 +1,67 @@
+class Node:
+    def __init__(self, val, key):
+        self.next = None
+        self.prev = None
+        self.val = val
+        self.key = key
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.keys = {}
-        self.cache = deque()
+        self.cache = {}
+        self.first = Node(0, 0)
+        self.last = Node(0, 0)
+        self.first.next = self.last
+        self.last.prev = self.first
         self.capacity = capacity
 
+    def _move_to_front(self, node: Node):
+        prev_mru = self.first.next
+        self.first.next = node
+        node.prev = self.first
+        node.next = prev_mru
+        prev_mru.prev = node
+    
+    def _trim_tail(self):
+        if self.last.prev and self.last.prev.prev:
+            prev = self.last.prev.prev
+            prev.next = self.last
+            self.last.prev = prev
+    
+    def _remove(self, node: Node):
+        next_node = node.next
+        prev_node = node.prev
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
     def get(self, key: int) -> int:
-        if key in self.keys:
-            self.cache.remove(key)
-            self.cache.appendleft(key)
-            return self.keys[key]
-        return -1
+        print(key, "get")
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._move_to_front(node)
+            print(node.val)
+            return node.val
+        return - 1
         
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.keys and self.capacity - len(self.keys) < 1:
-            lru = self.cache.pop()
-            del self.keys[lru]
-        
-        self.keys[key] = value
+        print(key, "put")
+        if key not in self.cache and len(self.cache) + 1 > self.capacity:
+            lru = self.last.prev
+            del self.cache[lru.key]
+            self._trim_tail()
+
         if key in self.cache:
-            self.cache.remove(key)
-        self.cache.appendleft(key)
+            node = self.cache[key]
+            self._remove(node)
+            node.val = value
+        else:
+            node = Node(value, key)
+            self.cache[key] = node
+        self._move_to_front(node)
+        
+
 
         
 
