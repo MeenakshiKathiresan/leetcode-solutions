1494dd2740d98afeaf6a9164f1997d1c0db0cce5
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-02-15 12:14:26 -0500

    Time: 3149 ms (5%), Space: 78 MB (95.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,42 +1,28 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.kvp = {}
-        self.kvp_access = []
+        self.keys = {}
+        self.cache = deque()
         self.capacity = capacity
-        
-        
 
     def get(self, key: int) -> int:
-        # update access
-        
-        if key in self.kvp:
-        
-            # find that value - pop and append
-            recently_used = self.kvp_access.pop(self.kvp_access.index(key))
-            self.kvp_access.append(recently_used)
-            return self.kvp[key]
-        else:
-            return -1
+        if key in self.keys:
+            self.cache.remove(key)
+            self.cache.appendleft(key)
+            return self.keys[key]
+        return -1
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.kvp:
-            recently_used = self.kvp_access.pop(self.kvp_access.index(key))
-            self.kvp_access.append(recently_used)
-            self.kvp[key] = value
-            
-        elif len(self.kvp) < self.capacity :
-            self.kvp[key] = value
-            self.kvp_access.append(key)
-            
-        else:
-            # remove the least used
-            self.kvp.pop(self.kvp_access[0])
-            self.kvp_access.pop(0)
-            self.kvp[key] = value
-            
-            self.kvp_access.append(key)
+        if key not in self.keys and self.capacity - len(self.keys) < 1:
+            lru = self.cache.pop()
+            del self.keys[lru]
+        
+        self.keys[key] = value
+        if key in self.cache:
+            self.cache.remove(key)
+        self.cache.appendleft(key)
+
         
 
 
