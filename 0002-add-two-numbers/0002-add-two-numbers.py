a10119d352b85bccbafa0266d7c1cf1cea6cc51a
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-02-15 08:06:03 -0500

    Time: 0 ms (100%), Space: 17.9 MB (35.25%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,48 +5,20 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-    
-        r = 0
         head = ListNode(0, None)
         rhead = head
-        last = None
-        while l1 and l2:
-            
-            res = l1.val + l2.val + r
-            head.val = res % 10
-            r = res // 10
-            head.next = ListNode(0, None)
+        r = 0
 
-            last = head
+        while l1 or l2 or r!=0:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            res = val1 + val2 + r
+            head.next = ListNode(res % 10, None)
             head = head.next
-            l1 = l1.next
-            l2 = l2.next
-
-        
-        while l1:
-            res = l1.val + r
-            head.val = res % 10
             r = res // 10
 
-            head.next = ListNode(0, None)
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
 
-            last = head
-            l1 = l1.next
-            head = head.next
-
-        while l2:
-            res = l2.val + r
-            head.val = res % 10
-            r = res // 10
-
-            last = head
-            head.next = ListNode(0, None)
-            head = head.next
-            l2 = l2.next
-
-        if r != 0:
-            head.val = r
-        else:
-            last.next = None
-        return rhead
+        return rhead.next
