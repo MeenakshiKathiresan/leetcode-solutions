de52d9e241e2988712a28e75b6306175e606fe51
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-06-29 16:44:40 -0400

    Time: 323 ms (8.81%), Space: 18.2 MB (23.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,22 +1,28 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        """
+        1. Repeat upto 26 times - maybe a set with all ch in s
+        2. Sliding window - start at 0 and end also at 0
+        3. move end until i can accomodate, once I can not, move start forward until 
+        non matching char is removed
+        4. res => get max
+
+        "AABABB"
+        """
         char_set = set(s)
         res = 0
 
         for letter in char_set:
             start = 0
             swappable = k
-
             for end in range(len(s)):
                 if s[end] != letter:
                     swappable -= 1
-
-                # If we used more than k replacements, shrink window
+                    
                 while swappable < 0:
                     if s[start] != letter:
                         swappable += 1
                     start += 1
-
-                res = max(res, end - start + 1)
-
+                res = max(res, end + 1 - start)
         return res
+                
