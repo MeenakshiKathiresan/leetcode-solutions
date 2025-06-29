596095736b3af6009224aea8f12767d52610e65b
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-06-29 07:45:17 -0400

    Time: 13 ms (56.84%), Space: 19.2 MB (68.89%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,26 +4,36 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
-        if len(height)==0: return 0
-        
+        # 2 pointers - 2 walls
+        # choose the lower of the two - definitely guarded
+        # add prev wall - curr height to trapped water and move
+
+        """
+
+        """
+
         left = 0
         right = len(height) - 1
-        
-        lmax = height[left]
-        rmax = height[right]
-        
-        ans = 0
-        
-        while left < right:
-            if lmax < rmax:
+
+        l_wall_ht = 0
+        r_wall_ht = 0
+
+        res = 0
+
+        while left <= right:
+            if l_wall_ht <= r_wall_ht:
+                if height[left] < l_wall_ht:
+                    res += l_wall_ht - height[left]
+
+                l_wall_ht = max(l_wall_ht, height[left])
                 left += 1
-                lmax = max(lmax, height[left])
-                ans += (lmax - height[left])
-            
-            else:
-                right -= 1
-                rmax = max(rmax, height[right])
-                ans += rmax - height[right]
                 
-        return ans
+            else:
+                if height[right] < r_wall_ht:
+                    res += r_wall_ht - height[right]
+
+                r_wall_ht = max(r_wall_ht, height[right])
+                right -= 1
+
+        print(right, left)
+        return res
