4c1158e5890970f61239a4b4184bd8cce78cb7fc
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-06-29 16:48:28 -0400

    Time: 26 ms (48.74%), Space: 18.1 MB (18.83%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -4,14 +4,8 @@ class Solution:
         s2_len = len(s2)
         s1_map = Counter(s1)
         s2_map = Counter(s2[:s1_len])
-
-        def dict_compare():
-            for k, v in s1_map.items():
-                if k not in s2_map or s2_map[k] != v:
-                    return False
-            return True
         
-        if dict_compare():
+        if s1_map == s2_map:
             return True
 
         for i in range(s1_len, s2_len, 1):
@@ -21,7 +15,7 @@ class Solution:
             s2_map[s2[i-s1_len]] -= 1
 
             if s2[i] in s1_map and s2_map[s2[i]] == s1_map[s2[i]]:
-                if dict_compare():
+                if s1_map == s2_map:
                     return True
 
         return False
