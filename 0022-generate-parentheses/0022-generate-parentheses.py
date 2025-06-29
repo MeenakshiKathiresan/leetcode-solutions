cda46a92d642475425f892b81301cec188d0f72b
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-06-29 11:17:32 -0400

    Time: 0 ms (100%), Space: 18 MB (43.42%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,29 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        """
-        At each step - either close if we have or create new
-        Create one -> open new or close existing
-
-        if we got all the brackets return res
-        """
-
         res = []
-        def recurse(left, stack, curr):
-            if left == 0:
-                while stack:
-                    curr += ")"
-                    stack -= 1
+
+        def recurse(left, right, curr):
+            if left == right == n:
                 res.append(curr)
                 return
-            
-            if left > 0:
-                recurse(left - 1, stack + 1, curr + "(")
-            
-            if stack > 0:
-                recurse(left, stack - 1, curr + ")")
-            
-        
-        recurse(n, 0, "")
+            if left < n:
+                recurse(left + 1, right, curr + "(")
+            if right < left:
+                recurse(left, right + 1, curr + ")")
+
+        recurse(0, 0, "")
         return res
-            
-            
