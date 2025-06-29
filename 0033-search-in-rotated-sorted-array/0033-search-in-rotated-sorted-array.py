8e9eb7899c74baa445b2eb426d69f1c854bb4a37
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-06-29 16:10:36 -0400

    Time: 0 ms (100%), Space: 18.1 MB (41.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,64 +1,25 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        # find pivot
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
 
-        # is given number greater than first number?
-        # if yes pivot is after that
-        # if no pivot is before that
+        while left <= right:
+            mid = (left + right)//2
 
-
-        # is number before or after pivot - binary search
-        low = 0
-        high = len(nums) - 1
-        mid = -1
-
-        while(low-1<high):
-            mid = low + ((high - low) /2)
-            print(mid, low, high)
-            if mid!=0 and nums[mid] < nums[mid -1]:
-                break
-            if nums[mid] > nums[0]:
-                low = mid + 1
-            elif nums[mid] < nums[0]:
-                high = mid - 1
-            else:
-
-                if len(nums)>1 and nums[mid] > nums[mid+1]:
-                    mid = mid + 1
-                break
-        
-
-        pivot = mid
-        print(pivot)
-        
-
-        if target < nums[0] or pivot ==0:
-            low = pivot
-            high = len(nums) -1
-        else:
-            low = 0
-            high = pivot
-        
-        
-        print(low, high)
-        # 1 2 3 4 5 6 7
-        while(low<high):
-            mid = low + ((high - low) /2)
-            print(mid, low, high)
-            if nums[mid] < target:
-                low = mid + 1
-            elif nums[mid] > target:
-                high = mid - 1
-            else:
+            if nums[mid] == target:
                 return mid
-
-        if low == high and nums[low] == target:
-            return low
-
-
+            # out of order in the right
+            if nums[mid] > nums[right]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else: # out of order in left
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return -1
+
+
+
