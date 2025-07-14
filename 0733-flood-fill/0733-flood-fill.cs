ae77330f030ebc221d966ff95299ac31c4d15764
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-07-14 15:34:26 -0400

    Time: 1 ms (49.06%), Space: 50.5 MB (12.5%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cs
+++ 0733-flood-fill/0733-flood-fill.cs
@@ -7,19 +7,18 @@ public class Solution {
         q.Enqueue((sr, sc));
         int m = image.Length;
         int n = image[0].Length;
-        image[sr][sc] = color;
 
         int[,] directions = new int[4,2] {{0,1},{0,-1},{1,0},{-1,0}};
 
         while(q.Count > 0){
             (int x, int y) curr =  q.Dequeue();
 
+            image[curr.x][curr.y] = color;
             for (int i = 0; i < 4; i++){
                 int newR = curr.x + directions[i, 0];
                 int newC = curr.y + directions[i, 1];
 
                 if (0 <= newR && newR < m && 0 <= newC && newC < n && image[newR][newC] == orig){
-                    image[newR][newC] = color; 
                      q.Enqueue((newR, newC));
                 }
             }
