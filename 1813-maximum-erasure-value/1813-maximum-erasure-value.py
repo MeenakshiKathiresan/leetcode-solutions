7d4f4d9a91b3e2f0291f2a7420ed3586c1cd9f61
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-07-21 21:35:26 -0400

    Time: 187 ms (87.17%), Space: 28.8 MB (50.6%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.py
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -6,15 +6,13 @@ class Solution:
         seen = set()
 
         for right, num in enumerate(nums):
-            # shrinking window when duplicate found
             while num in seen:
                 running_sum -= nums[left]
                 seen.remove(nums[left])
                 left += 1
-            
-            # expanding window
             running_sum += num
-            max_sum = max(max_sum, running_sum)
             seen.add(num)
+
+            max_sum = max(max_sum, running_sum)
 
         return max_sum
