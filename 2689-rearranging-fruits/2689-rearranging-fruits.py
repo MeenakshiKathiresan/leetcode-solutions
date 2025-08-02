c78e16dd1230b942eac26d643f1c690b3ccd17de
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-08-02 18:20:26 -0400

    Time: 95 ms (40.13%), Space: 46.6 MB (43.31%) - LeetHub

--- 2689-rearranging-fruits/2689-rearranging-fruits.py
+++ 2689-rearranging-fruits/2689-rearranging-fruits.py
@@ -1,3 +1,5 @@
+from collections import Counter
+from typing import List
 
 class Solution:
     def minCost(self, basket1: List[int], basket2: List[int]) -> int:
@@ -5,32 +7,18 @@ class Solution:
         b2 = Counter(basket2)
 
         moves = []
-        min_el = 999999999
+        min_el = float('inf')
 
-        for k in b1.keys():
-            min_el = min(k, min_el)
-            if (b1[k] + b2[k]) % 2 != 0:
-                return  -1
+        for k in set(b1.keys()).union(b2.keys()):
+            total = b1[k] + b2[k]
+            if total % 2 != 0:
+                return -1 
 
-            if k in b2 and b1[k] > b2[k]:
-                for i in range((b1[k] - b2[k])//2):
-                    moves.append(k)
-            elif k not in b2:
-                for i in range(b1[k]//2):
-                    moves.append(k)
-            
-        for k in b2.keys():
-            min_el = min(k, min_el)
-            if k in b1 and b2[k] > b1[k]:
-                for i in range((b2[k] - b1[k])//2):
-                    moves.append(k)
-            elif k not in b1:
-                for i in range(b2[k]//2):
-                    moves.append(k)
+            min_el = min(min_el, k)
+            diff = abs(b1[k] - b2[k])
+            moves.extend([k] * (diff // 2)) 
 
         moves.sort()
 
-        res = 0
-        for i in range(len(moves)//2):        
-            res += min(moves[i], 2 * min_el)
+        res = sum(min(x, 2 * min_el) for x in moves[:len(moves)//2])
         return res
