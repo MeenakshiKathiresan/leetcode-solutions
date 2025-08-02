cc411174ea5433d85dc34791412eab5021fe5768
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-08-02 18:18:14 -0400

    Time: 112 ms (19.75%), Space: 45 MB (50.96%) - LeetHub

--- 2689-rearranging-fruits/2689-rearranging-fruits.py
+++ 2689-rearranging-fruits/2689-rearranging-fruits.py
@@ -1,21 +1,37 @@
+
 class Solution:
     def minCost(self, basket1: List[int], basket2: List[int]) -> int:
-        freq = Counter()
-        m = float("inf")
-        for b1 in basket1:
-            freq[b1] += 1
-            m = min(m, b1)
-        for b2 in basket2:
-            freq[b2] -= 1
-            m = min(m, b2)
+        b1 = Counter(basket1)
+        b2 = Counter(basket2)
+        print(b1, b2)
 
-        merge = []
-        for k, c in freq.items():
-            if c % 2 != 0:
-                return -1
-            merge.extend([k] * (abs(c) // 2))
+        moves = []
+        min_el = 999999999
 
-        if not merge:
-            return 0
-        merge.sort()
-        return sum(min(2 * m, x) for x in merge[: len(merge) // 2])
+        for k in b1.keys():
+            min_el = min(k, min_el)
+            if (b1[k] + b2[k]) % 2 != 0:
+                return  -1
+
+            if k in b2 and b1[k] > b2[k]:
+                for i in range((b1[k] - b2[k])//2):
+                    moves.append(k)
+            elif k not in b2:
+                for i in range(b1[k]//2):
+                    moves.append(k)
+            
+        for k in b2.keys():
+            min_el = min(k, min_el)
+            if k in b1 and b2[k] > b1[k]:
+                for i in range((b2[k] - b1[k])//2):
+                    moves.append(k)
+            elif k not in b1:
+                for i in range(b2[k]//2):
+                    moves.append(k)
+
+        moves.sort()
+
+        res = 0
+        for i in range(len(moves)//2):        
+            res += min(moves[i], 2 * min_el)
+        return res
