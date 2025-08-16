de768a847726c1b0da84027a9508538a7db5826c
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-08-15 21:42:55 -0400

    Time: 0 ms (100%), Space: 17.6 MB (76.77%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.py
+++ 1448-maximum-69-number/1448-maximum-69-number.py
@@ -2,12 +2,9 @@ class Solution:
     def maximum69Number (self, num: int) -> int:
         
         num_str = str(num)
-        
         i = 0
         while i < len(num_str) and num_str[i] == "9":
             i += 1
-
-        print(i)
 
         if i == len(num_str):
             return num
