20b68e9b9eaf3a13fd7b451cf357e3a3621c4153
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2025-08-24 09:46:44 -0400

    Time: 27 ms (79.62%), Space: 28.9 MB (58.45%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,11 +1,17 @@
-class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        
-        seen, res = set(), set()
-        
-        for i in range(len(s)-9):
-            if s[i:i+10] in seen:
-                res.add(s[i:i+10])
-            seen.add(s[i:i+10])
-        return res
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        keys = set()
+        res = set()
+        for i in range(9, len(s), 1):
+            curr = s[i - 9: i + 1]
+            if curr in keys:
+                res.add(curr)
+            else:
+                keys.add(curr)
+        return list(res)
+
             
