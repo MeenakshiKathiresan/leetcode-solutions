0b2df6feeb3cfa774545da21ca768fcfbd63f6ce
Meenakshi Kathiresan <meenakshikathiresan3@gmail.com>
2026-07-13 12:30:03 -0700

    Time: 0 ms (100%), Space: 12.4 MB (50%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def sequentialDigits(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: List[int]
+        """
+
+        def digit_count(n):
+            cnt = 0
+            while n > 0:
+                n = n // 10
+                cnt += 1
+            return cnt
+
+        def generate(digit_count, min, max):
+            print(digit_count,min,max)
+            temp = []
+
+            for i in range(1, 10 - digit_count + 1):
+                curr_num = 0
+                for j in range(i, i + digit_count):
+                    curr_num = curr_num * 10 + j
+
+                if curr_num > max:
+                    return temp
+
+                if curr_num >= min:
+                    temp.append(curr_num)
+            return temp
+
+        res = []
+        low_count = digit_count(low)
+        high_count = digit_count(high) + 1
+
+        for i in range(low_count, high_count):
+            curr = generate(i, low, high)
+            res += curr
+
+        return res
